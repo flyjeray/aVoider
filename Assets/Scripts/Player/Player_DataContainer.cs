fcf5485c4f20205f4b9fb1e56e1f8eaa37410271
FlyJeRay <flyjeray.dev@gmail.com>
2020-07-18 11:33:25 +0300

    Initial commit

---
+++ Assets/Scripts/Player/Player_DataContainer.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_DataContainer : MonoBehaviour
+{
+    public float speed;
+    public bool isClockwiseDirectioned;
+}
---
+++ Assets/Scripts/Player/Player_Mechanics.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Требование компонента типа Player_DataContainer для корректной игры
+[RequireComponent(typeof(Player_DataContainer))]
+public class Player_Mechanics : MonoBehaviour
+{
+    private Player_DataContainer playerData;
+    [SerializeField] private Transform centerOfRotation;
+
+    private void Awake()
+    {
+        playerData = GetComponent<Player_DataContainer>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 direction;
+
+        if (Input.GetMouseButtonDown(0)) playerData.isClockwiseDirectioned = !playerData.isClockwiseDirectioned;
+
+        if (playerData.isClockwiseDirectioned) direction = Vector3.forward;
+        else direction = Vector3.back;
+
+        transform.RotateAround(centerOfRotation.position, direction, playerData.speed);
+    }
+}
